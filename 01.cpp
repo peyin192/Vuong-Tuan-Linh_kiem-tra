2e414fc37fa037400557b6004ab1ea3e8d1b6e75
peyin192 <vuonglinh190206.qv@gmail.com>
2024-11-21 18:06:04 +0700

    bt01

--- 01.cpp
+++ 01.cpp
@@ -16,16 +16,16 @@ void chuanhoaten(char *name) {
     char temp[end - start + 2];
     int j = 0;
     for (i = start; i <= end; i++) {
-        temp[j++] = name[i];
+        temp[j++] = name[i];//kiem tra co phai chu cai dau tien khong
     }
     temp[j] = '\0';
 
     int n = strlen(temp);
     for (i = 0; i < n; i++) {
         if (i == 0 || temp[i-1] == ' ') {
-            temp[i] = toupper(temp[i]);
+            temp[i] = toupper(temp[i]); //doi chu cai dau thanh chu in hoa
         } else {
-            temp[i] = tolower(temp[i]);
+            temp[i] = tolower(temp[i]); // doi cac chu cai khong phai chu cai dau thanh in thuong
         }
     }
 
@@ -37,13 +37,13 @@ int main() {
     scanf("%d\n", &t);
 
     for (int i = 0; i < t; i++) {
-        char name[81];
+        char name[81]; //ho ten co toi da 81 ki tu
         fgets(name, sizeof(name), stdin);
         name[strcspn(name, "\n")] = '\0';
         chuanhoaten(name);
         
-        chuanhoaten(name);
-        printf("ket qua: %s\n", str);
+        chuanhoaten(name); 
+        printf("ket qua: %s\n", name);// in ra chuan hoa ten
     }
 
     return 0;
